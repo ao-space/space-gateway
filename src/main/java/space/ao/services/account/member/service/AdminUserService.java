67d35e0d5074509c927f272dff9d21e9d20bb4ab
jiahao ren <r542251108@gmail.com>
2023-10-26 14:18:14 +0800

    .

--- src/main/java/space/ao/services/account/member/service/AdminUserService.java
+++ src/main/java/space/ao/services/account/member/service/AdminUserService.java
@@ -132,7 +132,6 @@ public class AdminUserService {
     userEntity.setClientUUID(adminBindInfo.getClientUUID());
     userEntity.setApplyEmail(adminBindInfo.getApplyEmail());
     userEntity.setPersonalName(adminBindInfo.getSpaceName());
-    //设置属性
     userEntity.setUserDomain(userRegistryResult.userDomain());
     userEntity.setAuthKey(authKey);
     userInfoRepository.insertAdminUser(userEntity);
