5b8dbb6a5369bfab20c0fcb3315fae5abec43e65
jiahao ren <r542251108@gmail.com>
2023-10-27 16:58:04 +0800

    migrate some entities from Java sdk to space gateway and add @RegisterForReflection annotation.

---
+++ src/main/java/space/ao/services/support/platform/model/ObtainBoxRegKeyResponse.java
@@ -0,0 +1,65 @@
+package space.ao.services.support.platform.model;
+
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
+@RegisterForReflection
+public class ObtainBoxRegKeyResponse  {
+
+    private String boxUUID;
+    private List<io.github.ren2003u.authentication.model.ObtainBoxRegKeyResponse.TokenResult> tokenResults = new ArrayList<>();
+
+    // Getters and setters with encapsulation
+
+    public String getBoxUUID() {
+        return this.boxUUID;
+    }
+
+    public void setBoxUUID(String boxUUID) {
+        this.boxUUID = boxUUID;
+    }
+
+    public List<io.github.ren2003u.authentication.model.ObtainBoxRegKeyResponse.TokenResult> getTokenResults() {
+        return new ArrayList<>(this.tokenResults);
+    }
+
+    public void setTokenResults(List<io.github.ren2003u.authentication.model.ObtainBoxRegKeyResponse.TokenResult> tokenResults) {
+        if (tokenResults != null) {
+            this.tokenResults = new ArrayList<>(tokenResults);
+        }
+    }
+    // Inner class for TokenResult
+    public static class TokenResult {
+        private String serviceId;
+        private String boxRegKey;
+        private OffsetDateTime expiresAt;
+
+        // getters and setters
+
+        public String getServiceId() {
+            return serviceId;
+        }
+
+        public void setServiceId(String serviceId) {
+            this.serviceId = serviceId;
+        }
+
+        public String getBoxRegKey() {
+            return boxRegKey;
+        }
+
+        public void setBoxRegKey(String boxRegKey) {
+            this.boxRegKey = boxRegKey;
+        }
+
+        public OffsetDateTime getExpiresAt() {
+            return expiresAt;
+        }
+
+        public void setExpiresAt(OffsetDateTime expiresAt) {
+            this.expiresAt = expiresAt;
+        }
+    }
+}
---
+++ src/main/java/space/ao/services/support/platform/model/RegisterUserResponse.java
@@ -0,0 +1,56 @@
+package space.ao.services.support.platform.model;
+
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
+@RegisterForReflection
+public class RegisterUserResponse {
+    private String boxUUID;
+    private String userId;
+    private String userDomain;
+    private String userType;
+    private String clientUUID;
+
+    // Getters and setters
+
+    public String getBoxUUID() {
+        return boxUUID;
+    }
+
+    public void setBoxUUID(String boxUUID) {
+        this.boxUUID = boxUUID;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserDomain() {
+        return userDomain;
+    }
+
+    public void setUserDomain(String userDomain) {
+        this.userDomain = userDomain;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
+    public String getClientUUID() {
+        return clientUUID;
+    }
+
+    public void setClientUUID(String clientUUID) {
+        this.clientUUID = clientUUID;
+    }
+
+
+}
